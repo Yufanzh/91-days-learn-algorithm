248f7f874b860334fc0af068308f72fbd6d1459a
Yufan <yfzhangpku@gmail.com>
2021-09-10 14:06:45 -0500

    feat: solution in python

--- day_01/solution.py
+++ day_01/solution.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        #use current = (num.current + k.current + carry)%10
+        #change K to list and then add
+        # or change num to int and add with K then change back to list
+        #method 2: add as number and then convert to list
+        return [int(i) for i in list(str(int("".join([str(i) for i in num]))+k))]
